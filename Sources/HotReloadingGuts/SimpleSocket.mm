3bba619504c638a5d2aaa2b599e44999a8e84849
jft0m <377632523@qq.com>
2024-12-15 21:36:14 +0800

    add peertalk on mac

--- Sources/HotReloadingGuts/SimpleSocket.mm
+++ Sources/HotReloadingGuts/SimpleSocket.mm
@@ -42,18 +42,62 @@ typedef union {
     struct sockaddr addr;
 } sockaddr_union;
 
+
+static const int PTExampleProtocolIPv4PortNumber = 2345;
+typedef struct _PTExampleTextFrame {
+  uint32_t length;
+  uint8_t utf8text[0];
+} PTExampleTextFrame;
+
+enum {
+  PTExampleFrameTypeDeviceInfo = 100,
+  PTExampleFrameTypeTextMessage = 101,
+  PTExampleFrameTypePing = 102,
+  PTExampleFrameTypePong = 103,
+};
+
+static const NSTimeInterval PTAppReconnectDelay = 1.0;
+
 @interface SimpleSocket (PT) <PTChannelDelegate>
 - (BOOL)connectViaPeerTalk;
 @end
 
-@interface SimpleSocket ()
+@interface SimpleSocket () {
+    NSNumber *connectingToDeviceID_;
+    NSNumber *connectedDeviceID_;
+    NSDictionary *connectedDeviceProperties_;
+    NSDictionary *remoteDeviceInfo_;
+    dispatch_queue_t notConnectedQueue_;
+    BOOL notConnectedQueueSuspended_;
+    PTChannel *connectedChannel_;
+    NSDictionary *consoleTextAttributes_;
+    NSDictionary *consoleStatusTextAttributes_;
+    NSMutableDictionary *pings_;
+}
 
 @property (nonatomic, weak) PTChannel *serverChannel;
 @property (nonatomic, weak) PTChannel *peerChannel;
 @property (nonatomic) dispatch_semaphore_t firstConnectSemaphore;
+
+@property (readonly) NSNumber *connectedDeviceID;
+@property PTChannel *connectedChannel;
+
+@end
+
+@interface SimpleSocket (PTMac)
+- (void)startListeningForDevices;
+- (void)didDisconnectFromDevice:(NSNumber*)deviceID;
+- (void)disconnectFromCurrentChannel;
+- (void)enqueueConnectToLocalIPv4Port;
+- (void)connectToLocalIPv4Port;
+- (void)connectToUSBDevice;
+- (void)ping;
+- (void)presentMessage:(NSString*)message isStatus:(BOOL)isStatus;
 @end
 
 @implementation SimpleSocket
+
+@synthesize connectedDeviceID = connectedDeviceID_;
 
 + (int)error:(NSString *)message {
     NSLog([@"%@/" stringByAppendingString:message],
@@ -63,6 +107,11 @@ typedef union {
 
 + (void)startServer:(NSString *)address {
     [self performSelectorInBackground:@selector(runServer:) withObject:address];
+    
+#ifdef INJECTION_III_APP
+    SimpleSocket *peertalkSocket = [[SimpleSocket alloc] initWithPeertalkMac];
+    
+#endif
 }
 
 + (void)forEachInterface:(void (^)(ifaddrs *ifa, in_addr_t addr, in_addr_t mask))handler {
@@ -195,6 +244,24 @@ typedef union {
 - (instancetype)initSocket:(int)socket {
     if ((self = [super init])) {
         clientSocket = socket;
+    }
+    return self;
+}
+
+
+- (instancetype)initWithPeertalkMac {
+    if ((self = [super init])) {
+        // Start listening for device attached/detached notifications on mac
+        [self startListeningForDevices];
+        
+        // Start trying to connect to local IPv4 port (defined in PTExampleProtocol.h)
+        [self enqueueConnectToLocalIPv4Port];
+        
+        // Put a little message in the UI
+        [self presentMessage:@"Ready for action — connecting at will." isStatus:YES];
+        
+        // Start pinging
+        [self ping];
     }
     return self;
 }
@@ -475,7 +542,7 @@ struct multicast_socket_packet {
 
 - (BOOL)connectViaPeerTalk {
     NSAssert(![NSThread isMainThread], @"should not call in main thread, peertalk will dispatch msg on main thread");
-    int result = dispatch_semaphore_wait(_firstConnectSemaphore, DISPATCH_TIME_FOREVER);
+    intptr_t result = dispatch_semaphore_wait(_firstConnectSemaphore, DISPATCH_TIME_FOREVER);
     return result;
 }
 
@@ -496,5 +563,229 @@ struct multicast_socket_packet {
 - (void)ioFrameChannel:(PTChannel *)channel didReceiveFrameOfType:(uint32_t)type tag:(uint32_t)tag payload:(nullable NSData *)payload {
     
 }
+
+@end
+
+@implementation SimpleSocket (PTMac)
+
+- (PTChannel*)connectedChannel {
+  return connectedChannel_;
+}
+
+- (void)setConnectedChannel:(PTChannel*)connectedChannel {
+  connectedChannel_ = connectedChannel;
+  
+  // Toggle the notConnectedQueue_ depending on if we are connected or not
+  if (!connectedChannel_ && notConnectedQueueSuspended_) {
+    dispatch_resume(notConnectedQueue_);
+    notConnectedQueueSuspended_ = NO;
+  } else if (connectedChannel_ && !notConnectedQueueSuspended_) {
+    dispatch_suspend(notConnectedQueue_);
+    notConnectedQueueSuspended_ = YES;
+  }
+  
+  if (!connectedChannel_ && connectingToDeviceID_) {
+    [self enqueueConnectToUSBDevice];
+  }
+}
+
+
+#pragma mark - Ping
+
+- (void)pongWithTag:(uint32_t)tagno error:(NSError*)error {
+  NSNumber *tag = [NSNumber numberWithUnsignedInt:tagno];
+  NSMutableDictionary *pingInfo = [pings_ objectForKey:tag];
+  if (pingInfo) {
+    NSDate *now = [NSDate date];
+    [pingInfo setObject:now forKey:@"date ended"];
+    [pings_ removeObjectForKey:tag];
+    NSLog(@"Ping total roundtrip time: %.3f ms", [now timeIntervalSinceDate:[pingInfo objectForKey:@"date created"]]*1000.0);
+  }
+}
+
+
+- (void)ping {
+  if (connectedChannel_) {
+    if (!pings_) {
+      pings_ = [NSMutableDictionary dictionary];
+    }
+    uint32_t tagno = [connectedChannel_.protocol newTag];
+    NSNumber *tag = [NSNumber numberWithUnsignedInt:tagno];
+    NSMutableDictionary *pingInfo = [NSMutableDictionary dictionaryWithObjectsAndKeys:[NSDate date], @"date created", nil];
+    [pings_ setObject:pingInfo forKey:tag];
+    [connectedChannel_ sendFrameOfType:PTExampleFrameTypePing tag:tagno withPayload:nil callback:^(NSError *error) {
+      [self performSelector:@selector(ping) withObject:nil afterDelay:1.0];
+      [pingInfo setObject:[NSDate date] forKey:@"date sent"];
+      if (error) {
+        [self->pings_ removeObjectForKey:tag];
+      }
+    }];
+  } else {
+    [self performSelector:@selector(ping) withObject:nil afterDelay:1.0];
+  }
+}
+
+
+#pragma mark - PTChannelDelegate
+- (BOOL)ioFrameChannel:(PTChannel*)channel shouldAcceptFrameOfType:(uint32_t)type tag:(uint32_t)tag payloadSize:(uint32_t)payloadSize {
+  if (   type != PTExampleFrameTypeDeviceInfo
+      && type != PTExampleFrameTypeTextMessage
+      && type != PTExampleFrameTypePing
+      && type != PTExampleFrameTypePong
+      && type != PTFrameTypeEndOfStream) {
+    NSLog(@"Unexpected frame of type %u", type);
+    [channel close];
+    return NO;
+  } else {
+    return YES;
+  }
+}
+
+- (void)ioFrameChannel:(PTChannel*)channel didReceiveFrameOfType:(uint32_t)type tag:(uint32_t)tag payload:(NSData *)payload {
+  //NSLog(@"received %@, %u, %u, %@", channel, type, tag, payload);
+  if (type == PTExampleFrameTypeDeviceInfo) {
+        NSDictionary *deviceInfo = [NSData dictionaryWithContentsOfData:payload];
+    [self presentMessage:[NSString stringWithFormat:@"Connected to %@", deviceInfo.description] isStatus:YES];
+  } else if (type == PTExampleFrameTypeTextMessage) {
+    PTExampleTextFrame *textFrame = (PTExampleTextFrame*)payload.bytes;
+    textFrame->length = ntohl(textFrame->length);
+    NSString *message = [[NSString alloc] initWithBytes:textFrame->utf8text length:textFrame->length encoding:NSUTF8StringEncoding];
+    [self presentMessage:[NSString stringWithFormat:@"[%@]: %@", channel.userInfo, message] isStatus:NO];
+  } else if (type == PTExampleFrameTypePong) {
+    [self pongWithTag:tag error:nil];
+  }
+}
+
+- (void)ioFrameChannel:(PTChannel*)channel didEndWithError:(NSError*)error {
+  if (connectedDeviceID_ && [connectedDeviceID_ isEqualToNumber:channel.userInfo]) {
+    [self didDisconnectFromDevice:connectedDeviceID_];
+  }
+  
+  if (connectedChannel_ == channel) {
+    [self presentMessage:[NSString stringWithFormat:@"Disconnected from %@", channel.userInfo] isStatus:YES];
+    self.connectedChannel = nil;
+  }
+}
+
+- (void)presentMessage:(NSString*)message isStatus:(BOOL)isStatus {
+  NSLog(@">> %@", message);
+}
+
+#pragma mark - Wired device connections
+
+
+- (void)startListeningForDevices {
+  NSNotificationCenter *nc = [NSNotificationCenter defaultCenter];
+  
+  [nc addObserverForName:PTUSBDeviceDidAttachNotification object:PTUSBHub.sharedHub queue:nil usingBlock:^(NSNotification *note) {
+    NSNumber *deviceID = [note.userInfo objectForKey:PTUSBHubNotificationKeyDeviceID];
+    //NSLog(@"PTUSBDeviceDidAttachNotification: %@", note.userInfo);
+    NSLog(@"PTUSBDeviceDidAttachNotification: %@", deviceID);
+
+    dispatch_async(self->notConnectedQueue_, ^{
+      if (!self->connectingToDeviceID_ || ![deviceID isEqualToNumber:self->connectingToDeviceID_]) {
+        [self disconnectFromCurrentChannel];
+                self->connectingToDeviceID_ = deviceID;
+                self->connectedDeviceProperties_ = [note.userInfo objectForKey:PTUSBHubNotificationKeyProperties];
+        [self enqueueConnectToUSBDevice];
+      }
+    });
+  }];
+  
+  [nc addObserverForName:PTUSBDeviceDidDetachNotification object:PTUSBHub.sharedHub queue:nil usingBlock:^(NSNotification *note) {
+    NSNumber *deviceID = [note.userInfo objectForKey:PTUSBHubNotificationKeyDeviceID];
+    //NSLog(@"PTUSBDeviceDidDetachNotification: %@", note.userInfo);
+    NSLog(@"PTUSBDeviceDidDetachNotification: %@", deviceID);
+    
+    if ([self->connectingToDeviceID_ isEqualToNumber:deviceID]) {
+            self->connectedDeviceProperties_ = nil;
+            self->connectingToDeviceID_ = nil;
+      if (self->connectedChannel_) {
+        [self->connectedChannel_ close];
+      }
+    }
+  }];
+}
+
+
+- (void)didDisconnectFromDevice:(NSNumber*)deviceID {
+  NSLog(@"Disconnected from device");
+  if ([connectedDeviceID_ isEqualToNumber:deviceID]) {
+    [self willChangeValueForKey:@"connectedDeviceID"];
+    connectedDeviceID_ = nil;
+    [self didChangeValueForKey:@"connectedDeviceID"];
+  }
+}
+
+
+- (void)disconnectFromCurrentChannel {
+  if (connectedDeviceID_ && connectedChannel_) {
+    [connectedChannel_ close];
+    self.connectedChannel = nil;
+  }
+}
+
+
+- (void)enqueueConnectToLocalIPv4Port {
+  dispatch_async(notConnectedQueue_, ^{
+    dispatch_async(dispatch_get_main_queue(), ^{
+      [self connectToLocalIPv4Port];
+    });
+  });
+}
+
+
+- (void)connectToLocalIPv4Port {
+  PTChannel *channel = [PTChannel channelWithDelegate:self];
+  channel.userInfo = [NSString stringWithFormat:@"127.0.0.1:%d", PTExampleProtocolIPv4PortNumber];
+  [channel connectToPort:PTExampleProtocolIPv4PortNumber IPv4Address:INADDR_LOOPBACK callback:^(NSError *error, PTAddress *address) {
+    if (error) {
+      if (error.domain == NSPOSIXErrorDomain && (error.code == ECONNREFUSED || error.code == ETIMEDOUT)) {
+        // this is an expected state
+      } else {
+        NSLog(@"Failed to connect to 127.0.0.1:%d: %@", PTExampleProtocolIPv4PortNumber, error);
+      }
+    } else {
+      [self disconnectFromCurrentChannel];
+      self.connectedChannel = channel;
+      channel.userInfo = address;
+      NSLog(@"Connected to %@", address);
+    }
+    [self performSelector:@selector(enqueueConnectToLocalIPv4Port) withObject:nil afterDelay:PTAppReconnectDelay];
+  }];
+}
+
+
+- (void)enqueueConnectToUSBDevice {
+  dispatch_async(notConnectedQueue_, ^{
+    dispatch_async(dispatch_get_main_queue(), ^{
+      [self connectToUSBDevice];
+    });
+  });
+}
+
+
+- (void)connectToUSBDevice {
+  PTChannel *channel = [PTChannel channelWithDelegate:self];
+  channel.userInfo = connectingToDeviceID_;
+  channel.delegate = self;
+  
+  [channel connectToPort:PTExampleProtocolIPv4PortNumber overUSBHub:PTUSBHub.sharedHub deviceID:connectingToDeviceID_ callback:^(NSError *error) {
+    if (error) {
+      if (error.domain == PTUSBHubErrorDomain && error.code == PTUSBHubErrorConnectionRefused) {
+        NSLog(@"Failed to connect to device #%@: %@", channel.userInfo, error);
+      } else {
+        NSLog(@"Failed to connect to device #%@: %@", channel.userInfo, error);
+      }
+      if (channel.userInfo == self->connectingToDeviceID_) {
+        [self performSelector:@selector(enqueueConnectToUSBDevice) withObject:nil afterDelay:PTAppReconnectDelay];
+      }
+    } else {
+            self->connectedDeviceID_ = self->connectingToDeviceID_;
+      self.connectedChannel = channel;
+    }
+  }];
+}
+
 @end
 #endif
